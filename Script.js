11c498c97ceabf3fdfee9105594b3b1f939822a9
Jamie Kielland <jamie@freshfx.no>
2020-03-31 12:29:41 +0200

    Update 1

--- Script.js
+++
@@ -1,3 +0,0 @@
-var date = [[0-9]];
-
-console.log(date);
---
+++ lib.js
@@ -0,0 +1,31 @@
+function isThisOk(date) {
+    return isDateLengthOk(date)
+    && isDotInThirdAndSeventhPlace(date)
+    && isYearAbove0000(date)
+    && isMonthBetween01And12(date)
+    && isDayBetween01And28(date)
+}
+
+//Teksten må ha lengde 10.
+function isDateLengthOk(date)   {
+    return date.length === 10;
+}
+//Det må være punktum i tredje posisjon og i sjette posisjon.
+function isDotInThirdAndSeventhPlace(date)  {
+    return date[2] == '.' && date[5] == '.'; 
+}
+//År er 0000 eller større. Kan sjekkes med year.length === 4 && year >= '0000' && year <= '9999'
+function isYearAbove0000(date)  {
+    var year = date.substring(6,10);
+    return year.length === 4 && year >= '0000' && year <= '9999'
+}
+//Måned er fra og med 01 til og med 12. Kan sjekkes som over.
+function isMonthBetween01And12(date)    {
+    var month = date.substring(3,5);
+    return month >= '01' && month <= '12' 
+}
+//Dag er fra og med 01 til og med 28
+function isDayBetween01And28(date)    {
+    var day = date.substring(0,2);
+    return day >= '01' && day <= '28'
+}
