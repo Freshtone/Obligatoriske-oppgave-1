f5ae4d0671acf37cf14d9b8d652d6ec35776e386
Jamie Kielland <jamie@freshfx.no>
2020-03-31 20:55:27 +0200

    update 6

--- lib.js
+++ lib.js
@@ -10,20 +10,24 @@ function isThisOk(date) {
 function isDateLengthOk(date)   {
     return date.length === 10;
 }
+
 //Det må være punktum i tredje posisjon og i sjette posisjon.
 function isDotInThirdAndSeventhPlace(date)  {
     return date[2] == '.' && date[5] == '.'; 
 }
+
 //År er 0000 eller større. Kan sjekkes med year.length === 4 && year >= '0000' && year <= '9999'
 function isYearAbove0000(date)  {
     var year = date.substring(6,10);
     return year.length === 4 && year >= '0000' && year <= '9999';
 }
+
 //Måned er fra og med 01 til og med 12. Kan sjekkes som over.
 function isMonthBetween01And12(date)    {
     var month = date.substring(3,5);
     return month >= '01' && month <= '12'; 
 }
+
 //Dag er fra og med 01 til og med 28
 function isDayBetween01And28(date)    {
     var day = date.substring(0,2);
@@ -39,7 +43,7 @@ function isFebuaryOk(date) {
         return true;
 }
 
+                function isLeapYear(year)   {
+                return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
+                }
 
-function isLeapYear(year)   {
-return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
-}
