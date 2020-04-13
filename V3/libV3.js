9237a8c6fd3798d97740c85eb21a6f05cce619ab
Jamie Kielland <jamie@freshfx.no>
2020-04-13 22:31:44 +0200

    Upodate

--- V3/libV3.js
+++ V3/libV3.js
@@ -9,15 +9,44 @@ function isThisOk(date) {
         && isDayBetween01And28(date)
 }
 
+//Teksten må ha lengde 10.
+function isDateLengthOk(date) {
+    return date.length === 10;
+}
 
-//run this function bellow with multiple functions attatched as above...
-//make variables instead of writing code directly in function.
+//Det må være punktum i tredje posisjon og i sjette posisjon.
+function isDotInThirdAndSeventhPlace(date) {
+    return date[2] == '.' && date[5] == '.';
+}
 
-function isDayOk(date) {
+//År er 0000 eller større. Kan sjekkes med year.length === 4 && year >= '0000' && year <= '9999'
+function isYearAbove0000(date) {
+    var year = date.substring(6, 10);
+    return year.length === 4 && year >= '0000' && year <= '9999';
+}
+
+//Måned er fra og med 01 til og med 12. Kan sjekkes som over.
+function isMonthBetween01And12(date) {
+    var month = date.substring(3, 5);
+    return month >= '01' && month <= '12';
+}
+
+//Dag er fra og med 01 til og med 28
+function isDayBetween01And28(date) {
+    var day = date.substring(0, 2);
+    return day >= '01' && day <= '28';
+}
+
+//29 er lov for februar hvis det er skuddår.
+//30 er lov for alle måneder utenom februar
+//31 er lovlig for januar, mars, mai, juli, august, oktober og desember
+function monthChecks(date) {
     var day = date.substring(0, 2);
     var month = date.substring(3, 5);
+    var year = date.substring(6,10);
     var isFebruary = month == '02';
     var isShortMonth = month == '04' || month ==  '06' || month ==  '09' || month ==  '11';
+    var isLeapYear = ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
     return day >= '01' && day <= '28'
       || day == '29' && (!isFebruary|| isLeapYear)
       || day == '30' && (!isFebruary)
